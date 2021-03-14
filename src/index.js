cfc816cf4607435f7d738107c66e3eed1e3848dc
taccianka <tacciankastudy@gmail.com>
2021-03-14 14:45:07 +0300

    fix: fix test function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-/**module.exports = */ function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
     // your solution
 
-    let stri = "([{(}])()";
+   /* let stri = "([{(}])()";
     const bracketsConfig1 = [
         ["(", ")"],
         ["[", "]"],
         ["{", "}"],
-    ];
-    let stringArray = stri.split("");
+    ];*/
+    let stringArray = str.split("");
     let tempArray = [];
     let k = 0;
     let result;
@@ -29,13 +29,13 @@
     }
 */
 
-    for (let j = 0; j < bracketsConfig1.length; j++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
         for (i = 0; i < stringArray.length; i++) {
-            if (stringArray[i] == bracketsConfig1[j][0]) {
+            if (stringArray[i] == bracketsConfig[j][0]) {
                 tempArray.push(stringArray[i]);
             } else if (
-                stringArray[i] == bracketsConfig1[j][1] &&
-                tempArray[tempArray.length - 1] == bracketsConfig1[j][0]
+                stringArray[i] == bracketsConfig[j][1] &&
+                tempArray[tempArray.length - 1] == bracketsConfig[j][0]
             ) {
                 tempArray.pop();
             } else continue;
@@ -61,7 +61,7 @@
                 tempArray.pop();
             }*/
 
-    if (
+  /*  if (
         typeof tempArray != "undefined" &&
         tempArray != null &&
         tempArray.length > 0
@@ -70,5 +70,13 @@
     } else result = true;
 
     console.log("tempArray = ", tempArray);
-    console.log("Result = ", result);
+    console.log("Result = ", result);*/
+
+    if (
+        typeof tempArray != "undefined" &&
+        tempArray != null &&
+        tempArray.length > 0
+    ) {
+        return false;
+    } else return true;
 }
