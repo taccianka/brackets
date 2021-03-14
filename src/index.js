0b76546aa7b874eea81f039a29c1cfc04de806b6
Tatyana <79045855+taccianka@users.noreply.github.com>
2021-03-15 02:04:52 +0300

    fix: fix case with numbers

--- src/index.js
+++ src/index.js
@@ -3,26 +3,33 @@ module.exports = function check(str, bracketsConfig) {
 
     let stringArray = str.split("");
     let tempArray = [];
-    let k = 0;
     let result;
-    let i, temp, flag;
+    let i, flag;
     
     flag = 1;
 
     for (i = 0; i < stringArray.length; i++) {
         for (let j = 0; j < bracketsConfig.length; j++) {
-            if (stringArray[i] == bracketsConfig[j][0] && (bracketsConfig[j][0] != bracketsConfig[j][1] || flag == 1)) {
+            if (
+                stringArray[i] == bracketsConfig[j][0] &&
+                (bracketsConfig[j][0] != bracketsConfig[j][1] || flag == 1)
+            ) {
                 tempArray.push(stringArray[i]);
                 if (bracketsConfig[j][0] == bracketsConfig[j][1]) flag = 0;
+                continue;
             } else if (stringArray[i] == bracketsConfig[j][1]) {
-                if (tempArray[tempArray.length - 1] != bracketsConfig[j][0] && bracketsConfig[j][0] != bracketsConfig[j][1]) {
+                if (
+                    tempArray[tempArray.length - 1] != bracketsConfig[j][0] &&
+                    bracketsConfig[j][0] != bracketsConfig[j][1]
+                ) {
+                    console.log("tempArray = ", tempArray);
+                    console.log("i = ", i);
                     return false;
                 }
                 if (tempArray[tempArray.length - 1] == bracketsConfig[j][0]) {
                     tempArray.pop();
-                   /* flag = 0;*/
+                    if (bracketsConfig[j][0] == bracketsConfig[j][1]) flag = 1;
                 }
-                if (bracketsConfig[j][0] == bracketsConfig[j][1]) flag = 1;
             } else continue;
         }
     }
